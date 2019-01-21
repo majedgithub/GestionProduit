fdba4aefd10f4874aa35da0120680a46e6a06786
majed ben ali <mr.majedbenali@gmail.com>
2019-01-21 11:06:58 +0100

    first commit

---
+++ src/main/java/org/sid/gestionproduit/GestionProduitApplication.java
@@ -0,0 +1,14 @@
+package org.sid.gestionproduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitApplication.class, args);
+	}
+
+}
+
